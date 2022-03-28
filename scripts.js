80c3227d040bde67a266738a80105ad9def2adf1
Carlos Diones <carlosdiones28@gmail.com>
2022-03-28 16:17:47 -0300

    interatividade na secao de parceiros

--- scripts.js
+++ scripts.js
@@ -18,3 +18,20 @@ $("#depoimentos-btn4").on("click", function(){
     $("#depoimento4").addClass("visible");
     $("#depoimento1, #depoimento2, #depoimento3").removeClass("visible");
 });
+
+// Interação com as marcas parceiras
+$("#parceiros-btn1").on("click", function(){
+    $("#brands-set1").addClass("visible");
+    $("#brands-set2, #brands-set3").removeClass("visible");
+});
+
+$("#parceiros-btn2").on("click", function(){
+    $("#brands-set2").addClass("visible");
+    $("#brands-set1, #brands-set3").removeClass("visible");
+});
+
+$("#parceiros-btn3").on("click", function(){
+    $("#brands-set3").addClass("visible");
+    $("#brands-set1, #brands-set2").removeClass("visible");
+});
+
